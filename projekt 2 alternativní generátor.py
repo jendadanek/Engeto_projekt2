84c9239a0d34f637c62b5bf5870dc94078319fae
jendadanek <95927762+jendadanek@users.noreply.github.com>
2022-01-11 00:09:06 +0100

    Add files via upload

---
+++ projekt 2 alternativní generátor.py
@@ -0,0 +1,44 @@
+from random import choice
+def generator_cisel():
+    cisla_do_deviti = []
+    for číslo in range(0,10):
+        cisla_do_deviti.append(číslo)
+    cislo1 = choice(cisla_do_deviti[1:])
+    cisla_do_deviti.remove(cislo1)
+    cislo2 = choice(cisla_do_deviti)
+    cisla_do_deviti.remove(cislo2)
+    cislo3 = choice(cisla_do_deviti)
+    cisla_do_deviti.remove(cislo3)
+    cislo4 = choice(cisla_do_deviti)
+    
+    tajenka = str(cislo1) + str(cislo2) + str(cislo3) + str(cislo4)
+    print(tajenka)
+
+
+generator_cisel()  
+
+
+
+
+
+        
+
+
+
+
+        
+
+    
+
+    
+
+
+
+
+
+
+        
+
+    
+            
+    
